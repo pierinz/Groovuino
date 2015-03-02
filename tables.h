0d9135f77a440a39bb652d5e247366fbfa1ecbb5
Gabriele Martino <g.martino@gmx.com>
2015-03-03 00:36:19 +0100

    Allow definition overrides in the main sketch

--- tables.h
+++ tables.h
@@ -1,18 +1,20 @@
-#define SAMPLE_RATE 44100.0 
-#define SAMPLES_PER_CYCLE 600 
-#define SAMPLES_PER_CYCLE_FIXEDPOINT (SAMPLES_PER_CYCLE<<20) 
-#define TICKS_PER_CYCLE (float)((float)SAMPLES_PER_CYCLE_FIXEDPOINT/(float)SAMPLE_RATE) 
+#ifndef SAMPLE_RATE
+#define SAMPLE_RATE 44100.0
+#endif
+#define SAMPLES_PER_CYCLE 600
+#define SAMPLES_PER_CYCLE_FIXEDPOINT (SAMPLES_PER_CYCLE<<20)
+#define TICKS_PER_CYCLE (float)((float)SAMPLES_PER_CYCLE_FIXEDPOINT/(float)SAMPLE_RATE)
 
-#define WAVE_SAMPLES 600 
-int16_t nSineTable[WAVE_SAMPLES]; 
-int16_t nSawTable[WAVE_SAMPLES]; 
+#define WAVE_SAMPLES 600
+int16_t nSineTable[WAVE_SAMPLES];
+int16_t nSawTable[WAVE_SAMPLES];
 int16_t nSqTable[WAVE_SAMPLES];
 
-#define MIDI_NOTES 128 
-uint32_t nMidiPhaseIncrement[MIDI_NOTES]; 
+#define MIDI_NOTES 128
+uint32_t nMidiPhaseIncrement[MIDI_NOTES];
 
 
-void createSineTable() 
+void createSineTable()
 { 
   for(uint32_t nIndex = 0;nIndex  < WAVE_SAMPLES;nIndex++) 
   { 
